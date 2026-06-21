52f741bd86e102898e3bf608d9d44a9dcf7c54b4
Vambah Sillah <37026900+vsillah@users.noreply.github.com>
2026-06-21 19:41:59 -0400

    Make welcome intro handoff non-blocking (#114)
    Co-authored-by: Vambah Sillah <vambahsillah@Vambahs-MacBook-Pro.local>

--- app/index.tsx
+++ app/index.tsx
@@ -51,7 +51,7 @@ import { ReviewerApprovalRecord } from "../utils/reviewerApprovalRecords";
 import { useCommercialization } from "../hooks/useCommercialization";
 import { formatJourneyCreditShortLabel, formatResetCountdown } from "../utils/commercialUsage";
 
-const WELCOME_INTRO_ENABLED = false;
+const WELCOME_INTRO_ENABLED = process.env.EXPO_PUBLIC_ENABLE_WELCOME_INTRO === 'true';
 
 interface MutationContext {
   id: string;
